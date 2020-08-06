146810167d9403341fa7044e2ae1fa4dea0447dc
SAMPATH <shyamalasampath484@gmail.com>
2020-08-06 13:16:27 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavanproject/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavanproject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	@Test(priority=0)
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	@Test
+	public void iosApps()
+	{
+		System.out.println("iosApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavanproject/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavanproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest
+{
+@Test
+public void browserAutomation()
+{
+	System.out.println("browserAutomation");
+}
+@Test
+public void elementUI()
+{
+	System.out.println("elementUI");
+}
+}
